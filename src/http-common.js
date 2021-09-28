ca62ff997dbdd15c71ecf568f77cc4e3f17666e4
Dmitry <diskibin@gmail.com>
2021-09-28 15:11:33 +0300

    DB connection fix

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://45.147.179.160:3000/api",
   headers: {
     "Content-type": "application/json"
   }
